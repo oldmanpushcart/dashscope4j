629c43ee7d3b0acad73e7097c7baf6e3b8b783a6
dukun <dukun@alibaba-inc.com>
2025-11-12 18:18:08 +0800

    更新代码

--- dashscope4j-client/src/main/java/io/github/oldmanpushcart/dashscope4j/client/internal/api/chat/FunctionToolCallOpFlowHandler.java
+++ dashscope4j-client/src/main/java/io/github/oldmanpushcart/dashscope4j/client/internal/api/chat/FunctionToolCallOpFlowHandler.java
@@ -29,12 +29,14 @@ class FunctionToolCallOpFlowHandler implements UnaryOperator<Flow.Publisher<Chat
         return downstream -> source.subscribe(new ToolCallSubscriber(downstream));
     }
 
+
     private class ToolCallSubscriber implements Flow.Subscriber<ChatResponse> {
 
         private final Flow.Subscriber<? super ChatResponse> downstream;
-        private final List<ToolCallMessage> tcMessages = new ArrayList<>();
+        private final List<ToolCallMessage> tcMessageSegments = new ArrayList<>();
+
         private Flow.Subscription upstream;
-        private volatile boolean forwarded = false;
+        private ChatRequest request;
 
         private ToolCallSubscriber(Flow.Subscriber<? super ChatResponse> downstream) {
             this.downstream = downstream;
@@ -60,44 +62,62 @@ class FunctionToolCallOpFlowHandler implements UnaryOperator<Flow.Publisher<Chat
 
         @Override
         public void onNext(ChatResponse response) {
+            try {
+
+                /*
+                 * 补充上ChatRequest
+                 * 整个流中取第一个即可，整个流的都是同一个request
+                 */
+                if (null == request) {
+                    request = (ChatRequest) response.request();
+                }
+
+                /*
+                 * 如果有ToolCallMessage，则讲片段缓存起来
+                 * 在onCompleted的时候再合并起来使用
+                 */
+                final var choice = response.output().best();
+                final var message = choice.message();
+                if (message instanceof ToolCallMessage tcMessage) {
+                    tcMessageSegments.add(tcMessage);
+                } else {
+                    downstream.onNext(response);
+                }
+
+                upstream.request(1);
+
+            } catch (Throwable ex) {
+                downstream.onError(ex);
+            }
+        }
+
+        @Override
+        public void onError(Throwable ex) {
+            downstream.onError(ex);
+        }
+
+        private ToolCallMessage parseToolCallMessage() {
+            if (null == request) {
+                return null;
+            }
+            final var incremental = request.parameters().has(ENABLE_INCREMENTAL_OUTPUT, true);
+            return tcMessageSegments.stream()
+                    .reduce((c1, c2) -> incremental ? c1.accumulate(c2) : c2)
+                    .orElse(null);
+        }
+
+        @Override
+        public void onComplete() {
 
             try {
 
-                final var request = (ChatRequest) response.request();
-                final var incremental = request.parameters().has(ENABLE_INCREMENTAL_OUTPUT, true);
-                final var choice = response.output().best();
-                final var finish = choice.finish();
-                final var message = choice.message();
-
-                // 如果是普通消息，则直接下发
-                if (!(message instanceof ToolCallMessage tcMessage)) {
-                    downstream.onNext(response);
-                    upstream.request(1);
-                    return;
-                }
-
-                // 工具调用消息则缓存起来，准备进行合并后拼接，方便后续工具调用
-                tcMessages.add(tcMessage);
-
-                // 遇到最后一个工具调用消息，则进行合并，并发起工具调用
-                if (finish == ChatResponse.Finish.TOOL_CALLS) {
-
-                    // 合并为最终的TcMessage
-                    final var finishTcMessage = tcMessages.stream()
-                            .reduce((c1, c2) -> incremental ? c1.accumulate(c2) : c2)
-                            .orElseThrow(() -> new IllegalStateException("Non tool call message collected!"));
-
-                    /*
-                     * 标记当前流已被转发到子流
-                     * 当流被转发后，流的完成将由子流触发
-                     */
-                    forwarded = true;
-
-                    /*
-                     * 发起工具调用，产生子流
-                     * 并将工具调用产生的流转发到下游接收方
-                     */
-                    new FunctionToolCaller(chatOp, request, finishTcMessage)
+                /*
+                 * 发起工具调用，产生子流
+                 * 并将工具调用产生的流转发到下游接收方
+                 */
+                final var tcMessage = parseToolCallMessage();
+                if (null != tcMessage) {
+                    new FunctionToolCaller(chatOp, request, tcMessage)
                             .flowCall()
                             .thenAccept(publisher -> {
                                 final var forwarding = new ForwardingSubscriber(downstream);
@@ -107,33 +127,14 @@ class FunctionToolCallOpFlowHandler implements UnaryOperator<Flow.Publisher<Chat
                                 downstream.onError(ex);
                                 return null;
                             });
-
-                } else {
-                    upstream.request(1);
+                    return;
                 }
 
             } catch (Throwable ex) {
-                downstream.onError(ex);
+                onError(ex);
             }
 
-        }
-
-        @Override
-        public void onError(Throwable ex) {
-            downstream.onError(ex);
-        }
-
-        @Override
-        public void onComplete() {
-
-            /*
-             * 如果当期流已经被转到子流，则主流结束并不会触发下游玩完成。
-             * 下游完成将会由子流处理
-             */
-            if (!forwarded) {
-                downstream.onComplete();
-            }
-
+            downstream.onComplete();
         }
 
     }
