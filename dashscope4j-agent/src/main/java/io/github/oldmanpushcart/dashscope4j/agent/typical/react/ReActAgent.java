a9920f9d74c491c9fb37a60298b63d9ad59911b3
dukun <dukun@alibaba-inc.com>
2026-04-01 13:21:03 +0800

    重构
    # Conflicts:
    #	dashscope4j-agent/src/main/java/io/github/oldmanpushcart/dashscope4j/agent/repository/tool/loader/mcp/McpToolLoader.java
    #	dashscope4j-agent/src/main/java/io/github/oldmanpushcart/dashscope4j/agent/repository/tool/loader/skill/SkillToolLoader.java
    #	dashscope4j-agent/src/test/java/io/github/oldmanpushcart/dashscope4j/agent/DebugTestCase.java

--- dashscope4j-agent/src/main/java/io/github/oldmanpushcart/dashscope4j/agent/typical/react/ReActAgent.java
+++ dashscope4j-agent/src/main/java/io/github/oldmanpushcart/dashscope4j/agent/typical/react/ReActAgent.java
@@ -1,14 +1,43 @@
 package io.github.oldmanpushcart.dashscope4j.agent.typical.react;
 
+import io.github.oldmanpushcart.dashscope4j.agent.repository.Repository;
 import io.github.oldmanpushcart.dashscope4j.agent.typical.BaseAgent;
+import io.github.oldmanpushcart.dashscope4j.client.aigc.chat.tool.Tool;
+import io.github.oldmanpushcart.dashscope4j.client.api.interceptor.Interceptor;
+import io.github.oldmanpushcart.dashscope4j.client.util.CommonUtils;
+
+import java.util.List;
 
 public class ReActAgent extends BaseAgent {
 
+    private final Interceptor reActInterceptor;
+
     protected ReActAgent(Builder builder) {
         super(builder);
+        this.reActInterceptor = new ReActInterceptor(
+                builder.toolRepository
+        );
+    }
+
+    @Override
+    protected List<Interceptor> interceptors() {
+        final var interceptors = CommonUtils.mutableCopy(super.interceptors());
+        interceptors.add(reActInterceptor);
+        return interceptors;
+    }
+
+    public static Builder newBuilder() {
+        return new Builder();
     }
 
     public static class Builder extends BaseAgent.Builder<ReActAgent, ReActAgent.Builder> {
+
+        private Repository<String, Tool> toolRepository;
+
+        public Builder tool(Repository<String, Tool> toolRepository) {
+            this.toolRepository = toolRepository;
+            return this;
+        }
 
         @Override
         public ReActAgent build() {
